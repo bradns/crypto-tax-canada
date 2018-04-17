2f2241ef81bc853bd9b46f516fb6878319f8c259
Brad Smith <bnsmith@gmail.com>
2018-04-16 21:25:42 -0400

    Added some improvements to the Binance parsing capability.

--- transaction_parsing/binance_parser.py
+++ transaction_parsing/binance_parser.py
@@ -2,12 +2,16 @@ import pyexcel
 
 from transaction_parsing.universal_transaction import UniversalTransaction, TransactionType
 
+MARKETS = {"BNBETH": ("BNB", "ETH"), "BCCETH": ("BCC", "ETH"), "IOTAETH": ("IOTA", "ETH"),
+           "IOTABNB": ("IOTA", "BNB"), "VENETH": ("VEN", "ETH"), "ENGETH": ("ENG", "ETH")}
+
 
 def parse_xlsx(xlsx_filename):
     result = []
     records = pyexcel.iget_records(file_name=xlsx_filename)
     for r1 in list(records):
         tr_type = TransactionType.Buy if r1["Type"].lower() == "buy" else TransactionType.Sell
-        ut = UniversalTransaction(tr_type, "0", "0", "0", "0", "0")
+        major, minor = MARKETS[r1["Market"]]
+        ut = UniversalTransaction(tr_type, r1["Date"], major, minor, r1["Amount"], r1["Price"])
         result.append(ut)
     return result
