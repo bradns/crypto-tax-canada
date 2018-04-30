508d33a091370680c4e04e39cf20cdd89cf9e26e
Brad Smith <bnsmith@gmail.com>
2018-04-29 20:46:50 -0400

    Major overhaul to the module that looks up historical prices from an online API.

--- transaction_parsing/historical_info.py
+++ transaction_parsing/historical_info.py
@@ -13,21 +13,23 @@ class HistoricalInfo:
                 self.cached_prices = pickle.load(fin)
 
     def get_cad_price(self, symbol, dt):
-        if symbol in self.cached_prices:
-            return self.cached_prices[symbol]
+        if (symbol, dt) in self.cached_prices:
+            return self.cached_prices[(symbol, dt)]
         else:
             p = self.get_cad_price_webapi(symbol, dt)
-            self.cached_prices[symbol] = p
+            self.cached_prices[(symbol, dt)] = p
             with open(CACHE_PKL, 'wb') as fout:
                 pickle.dump(self.cached_prices, fout)
             return p
 
     def get_cad_price_webapi(self, symbol, dt):
+        api_symbol = symbol if symbol != 'IOTA' else 'IOT' # This API seems to require prices for IOTA to be checked with IOT...
+        api_symbol = api_symbol if symbol != 'BCC' else 'BCH' # A difference between the symbols used by binance and the API
         ts = int(dt.timestamp())
-        params = {'fsym': symbol, 'tsym': 'CAD', 'toTs': str(ts)}
-        r = requests.get(CRYPTOCOMPARE_URL + "dayAvg", params=params)
+        params = {'fsym': api_symbol, 'tsym': 'CAD', 'toTs': str(ts)}
+        r = requests.get(CRYPTOCOMPARE_URL + "histohour", params=params)
         if r.status_code != 200:
             raise Exception(f"ERROR: Unable to retrieve a price from the web-api: {r}")
         else:
             json = r.json()
-            return json['CAD']
+            return json['Data'][0]['low']
